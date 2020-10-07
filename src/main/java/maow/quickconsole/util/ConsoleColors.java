080b8e62e8fc6e34b47e38a8d8414dfa218006d4
Maow <28311382+Maowcraft@users.noreply.github.com>
2020-10-07 18:58:02 -0400

    Delete ConsoleColors.java

--- src/main/java/maow/quickconsole/util/ConsoleColors.java
+++
@@ -1,9 +0,0 @@
-package maow.quickconsole.util;
-
-public class ConsoleColors {
-    public static final String RESET = "\033[0m";
-    public static final String RED = "\033[0;31m";
-    public static final String YELLOW = "\033[0;33m";
-    public static final String WHITE = "\033[0;37m";
-    public static final String RED_BACKGROUND = "\033[41m";
-}
